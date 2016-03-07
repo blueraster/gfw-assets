41a401c4f38dffa9e41973de54904ca0d6b98839
David Inga <davidsingal@icloud.com>
2016-03-07 16:48:21 +0100

    added test without global libraries

--- test/specs/isolated_libs_spec.js
+++ test/specs/isolated_libs_spec.js
@@ -2,21 +2,21 @@
 
 describe('Isolated libraries', function() {
 
-  describe('global jQuery library', function() {
+  describe('local jQuery library', function() {
+    it('should exist', function() {
+      expect(window.GFW.$).to.exist;
+    });
+    it('should be "2.1.4" version', function() {
+      expect(window.GFW.$.fn.jquery).to.be.equal('2.1.4');
+    });
+  });
+
+  describe('global jQuery', function() {
     it('should exist', function() {
       expect(window.$).to.exist;
     });
     it('should be "1.10.2" version', function() {
       expect(window.$.fn.jquery).to.be.equal('1.10.2');
-    });
-  });
-
-  describe('local jQuery library', function() {
-    it('should exist', function() {
-      expect(window.$gfw).to.exist;
-    });
-    it('should be "2.2.1" version', function() {
-      expect(window.$gfw.fn.jquery).to.be.equal('2.2.1');
     });
   });
 
