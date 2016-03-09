7ea8c4ba81ce9bed0e5e4a1ec99a810eb13a1ff3
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2016-03-09 16:21:32 +0100

    Fonts load bulletproof

--- src/main.js
+++ src/main.js
@@ -8,10 +8,12 @@
   'use strict';
 
   // At begining add google fonts
-  var linkTag = document.createElement('link');
-  linkTag.rel = 'stylesheet';
-  linkTag.href = 'https://fonts.googleapis.com/css?family=Fira+Sans:300,400,500';
-  document.head.appendChild(linkTag);
+  setTimeout(function() {  
+    var linkTag = document.createElement('link');
+    linkTag.rel = 'stylesheet';
+    linkTag.href = 'https://fonts.googleapis.com/css?family=Fira+Sans:300,400,500';
+    document.head.appendChild(linkTag);
+  },0);
 
   // Add styles
   require('./styles/global.scss');
