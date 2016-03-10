032dd56994f8373c9c2ec95d2d9d0ff28d28d56e
David Inga <davidsingal@icloud.com>
2016-03-10 18:06:04 +0100

    migrating main and footer to es6

--- src/main.js
+++ src/main.js
@@ -1,34 +1,31 @@
+'use strict';
+
 /**
  * Main file:
  * In this we are going to initialize all modules
  * @param  {window} root
  */
-(function() {
 
-  'use strict';
+import Header from './scripts/modules/header';
+import Footer from './scripts/modules/footer';
+import Feedback from './scripts/modules/feedback';
 
-  // At begining add google fonts asynchronously
-  setTimeout(function() {
-    var linkTag = document.createElement('link');
-    linkTag.rel = 'stylesheet';
-    linkTag.href = 'https://fonts.googleapis.com/css?family=Fira+Sans:300,400,500';
-    document.head.appendChild(linkTag);
-  },0);
+// Add styles
+import './styles/global.scss';
 
-  // Add styles
-  require('./styles/global.scss');
+// At begining add google fonts asynchronously
+setTimeout(() => {
+  let linkTag = document.createElement('link');
+  linkTag.rel = 'stylesheet';
+  linkTag.href = 'https://fonts.googleapis.com/css?family=Fira+Sans:300,400,500';
+  document.head.appendChild(linkTag);
+}, 0);
 
-  // Adding header
-  var Header = require('./scripts/modules/header');
-  new Header();
+// Adding header
+new Header();
 
-  // Adding footer
-  var Footer = require('./scripts/modules/footer');
-  new Footer();
+// Adding footer
+new Footer();
 
-  // Adding feedback
-  var Feedback = require('./scripts/modules/feedback');
-  new Feedback();
-
-
-}).call(this);
+// Adding feedback
+new Feedback();
