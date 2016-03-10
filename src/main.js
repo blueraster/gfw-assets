f295a2bebfa086e05bcf41cd405cf25a14c439c3
David Inga <davidsingal@icloud.com>
2016-03-10 13:32:17 +0100

    changed main comment

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@
 
   'use strict';
 
-  // At begining add google fonts
-  setTimeout(function() {  
+  // At begining add google fonts asynchronously
+  setTimeout(function() {
     var linkTag = document.createElement('link');
     linkTag.rel = 'stylesheet';
     linkTag.href = 'https://fonts.googleapis.com/css?family=Fira+Sans:300,400,500';
