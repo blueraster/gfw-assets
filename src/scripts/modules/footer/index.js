7818ad24f0c5be36433595b2919cad4fa30d4394
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2016-03-10 10:55:32 +0100

    Footer: lory slider, structure

--- src/scripts/modules/footer/index.js
+++ src/scripts/modules/footer/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
+var lory = require('../../libs/lory.min').lory.lory;
 var footerTpl = require('raw!./footer.tpl');
+var footerIconsTpl = require('raw!./footer-icons.tpl');
 
 /**
  * Footer
@@ -18,11 +20,43 @@ module.exports = function() {
   };
 
   this.render = function() {
-    this.el.innerHTML = footerTpl;
+    this.el.innerHTML = footerTpl + footerIconsTpl;
+    
+    this.initSlider();
+
     return this;
   };
 
-  this.initSlider = function() {};
+  // SLIDER
+  this.initSlider = function() {
+    this.sliderEl = document.querySelector('#my-gfw-slider');
+
+    // Events
+    this.sliderEl.addEventListener('before.lory.slide', this.cancelTimer.bind(this));
+    this.sliderEl.addEventListener('after.lory.slide', this.initTimer.bind(this));
+    this.sliderEl.addEventListener('mouseover', this.cancelTimer.bind(this));
+    this.sliderEl.addEventListener('mouseout', this.initTimer.bind(this));
+
+    this.slider = lory(this.sliderEl, {
+      infinite: 5,
+      slidesToScroll: 1,
+      slideSpeed: 500
+    });
+
+    this.initTimer();
+  };
+
+  this.initTimer = function() {
+    if (!this.sliderTimer) {
+      this.sliderTimer = setInterval(this.slider.next.bind(this.slider), 3000);    
+    }
+  };
+
+  // clear timer
+  this.cancelTimer = function() {
+    clearInterval(this.sliderTimer);
+    this.sliderTimer = null;
+  };
 
   this.init();
 
