2dda4764595704daaacdc278cfdb8821a3c6ed3e
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2016-03-11 13:19:02 +0100

    Menu: avoid using switch statement. Completely unnecessary

--- src/scripts/modules/navigation/index.js
+++ src/scripts/modules/navigation/index.js
@@ -9,6 +9,14 @@ import globaljson from './global.json';
 import climatejson from './climate.json';
 import howtojson from './howto.json';
 
+const navigationMenus = {
+  'default': globaljson,
+  'localhost': globaljson,
+  'climate.globalforestwatch.org': climatejson,
+  'howto.globalforestwatch.org': howtojson
+};
+
+
 /**
  * Navigation
  * @param  {window} root
@@ -21,33 +29,15 @@ class Navigation {
     if (!this.$el) {
       throw new Error('element #submenuMenuMobile doesn\'t exist');
     }
-    this.switchMenu();
-  }
-
-  switchMenu() {
-    switch(location.hostname) {
-      case 'localhost':
-        this.menu = globaljson;
-      break;
-
-      case 'climate.globalforestwatch.org':
-        this.menu = climatejson;
-      break;
-
-      case 'howto.globalforestwatch.org':
-        this.menu = howtojson;
-      break;
-
-      default:
-        this.menu = globaljson;
-    }
     this.render();
   }
 
-
   render() {
+    var menu = navigationMenus[location.hostname] || navigationMenus['default'];
+    // TO-DO: this is a little weird. I need to render the script template and then re-render it with the json
+    // That happens because the microtemplate plugin looks for an id, it would be great if we can pass directly the template...
     this.$el.html(navigationTpl);
-    this.$el.html(jstpl.template('mobileMenuTPL', { menu: this.menu}));
+    this.$el.html(jstpl.template('mobileMenuTPL', { menu: menu}));
   }
 
 }
