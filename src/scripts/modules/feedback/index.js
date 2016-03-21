251e4716090f635764dfb338944c3374f9164742
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2016-03-21 09:55:21 +0100

    Feedback: unnecessary code removed

--- src/scripts/modules/feedback/index.js
+++ src/scripts/modules/feedback/index.js
@@ -42,13 +42,6 @@ class Feedback {
   }
 
   initVars() {
-    const w = window,
-          d = document,
-          e = d.documentElement,
-          g = d.getElementsByTagName('body')[0],
-          x = w.innerWidth || e.clientWidth || g.clientWidth,
-          y = w.innerHeight|| e.clientHeight|| g.clientHeight;
-
     this.$window   =  window;
     this.$document =  $gfwdom(document);
     this.$body     =  $gfwdom('body');
