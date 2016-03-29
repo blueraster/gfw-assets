66b71e9de40d0d36ae07ae70fba735a46f6cb6cf
David Inga <davidsingal@icloud.com>
2016-03-29 10:11:44 +0200

    adding local fonts

--- src/main.js
+++ src/main.js
@@ -11,14 +11,17 @@ import Footer from './scripts/modules/footer';
 import Feedback from './scripts/modules/feedback';
 
 // Add styles
+import './styles/fonts.css';
 import './styles/global.scss';
 
 // At begining add google fonts asynchronously
 setTimeout(() => {
-  let linkTag = document.createElement('link');
-  linkTag.rel = 'stylesheet';
-  linkTag.href = 'https://fonts.googleapis.com/css?family=Fira+Sans:300,400,500';
-  document.head.appendChild(linkTag);
+  // let linkTag = document.createElement('link');
+  // linkTag.rel = 'stylesheet';
+  // linkTag.media = 'all';
+  // linkTag.href = 'fonts.css';
+  // linkTag.href = 'https://fonts.googleapis.com/css?family=Fira+Sans:300,400,500';
+  // document.head.appendChild(linkTag);
 }, 0);
 
 // Adding header
