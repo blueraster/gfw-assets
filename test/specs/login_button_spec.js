4a6153637564b28974976255252b0d7776b339f6
Adam Mulligan <adammulligan@gmail.com>
2016-03-31 13:15:28 +0100

    Set the global Google Analytics user ID when users are logged in

---
+++ test/specs/login_button_spec.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import LoginButton from '../../src/scripts/modules/my-gfw/login-button';
+
+describe('Login Button', function() {
+
+  describe('.setupGoogleAnalytics', function() {
+
+    it('sets the user ID on the global Google Analytics object', function() {
+      window.ga = sinon.spy();
+
+      let loginButton = new LoginButton();
+      loginButton.setupGoogleAnalytics({id: '123'});
+
+      expect(window.ga.calledWith('set', 'userId', '123')).to.be.true;
+    });
+
+  });
+
+});
