ca450f895df04cf8f23a92019972bfdc5360843f
Gerardo <gerardo.pacheco@vizzuality.com>
2016-10-21 11:42:45 +0200

    Feat: Logged users - Check for incomplete profiles and redirect to profile

--- src/scripts/modules/my-gfw/login-button.js
+++ src/scripts/modules/my-gfw/login-button.js
@@ -25,6 +25,7 @@ class LoginButton {
     // Comment this if you want to check it locally
     utils.isLoggedIn({
       success: function(response) {
+        this.checkCompleteProfile(response);
         this.loggedIn = true;
         this.render();
         this.setupGoogleAnalytics(response);
@@ -56,6 +57,14 @@ class LoginButton {
     $signin.forEach(function(v) {
       $gfwdom(v).attr('href', utils.getAPIHost(true) + '/' + $gfwdom(v).attr('href'))
     });
+  }
+
+  // Require profile completation
+  checkCompleteProfile(response) {
+    const profileComplete = response.attributes.profileComplete || true;
+    if (!profileComplete) {
+      window.location = '/my_gfw';
+    }
   }
 
   logout(e) {
