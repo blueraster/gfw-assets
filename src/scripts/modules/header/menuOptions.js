a3df1aba6522806b9e76a218150f86351f4af065
j8seangel <jseangel@gmail.com>
2017-10-23 16:59:31 +0200

    Check options before looping

--- src/scripts/modules/header/menuOptions.js
+++ src/scripts/modules/header/menuOptions.js
@@ -10,14 +10,16 @@ const menuOptions = {
     const navOptions = window.liveSettings.menuOptions;
     let siteStyles;
     let menuOptionsNav = '';
-    for (let i = 0; i < navOptions.options.length; i += 1) {
-      if (navOptions.options[i].title === 'logo') {
-        logoLink.setAttribute('href', navOptions.options[i].url);
-      } else {
-        menuOptionsNav += `
-          <li class="${path === navOptions.options[i].url || path === navOptions.options[i].url + '/' ? siteStyles : ''}" data-menu="${navOptions.options[i].url}">
-            <a class="option-menu" href="${navOptions.options[i].url}"> ${navOptions.options[i].title} </a>
-          </li>`;
+    if (navOptions && navOptions.options && navOptions.options.length) {
+      for (let i = 0; i < navOptions.options.length; i += 1) {
+        if (navOptions.options[i].title === 'logo') {
+          logoLink.setAttribute('href', navOptions.options[i].url);
+        } else {
+          menuOptionsNav += `
+            <li class="${path === navOptions.options[i].url || path === navOptions.options[i].url + '/' ? siteStyles : ''}" data-menu="${navOptions.options[i].url}">
+              <a class="option-menu" href="${navOptions.options[i].url}"> ${navOptions.options[i].title} </a>
+            </li>`;
+        }
       }
     }
     return menuOptionsNav;
